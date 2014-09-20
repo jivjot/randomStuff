41c18c717e785e542a123c1937d99852a2a78175
jivjot <jivjot@gmail.com>
2014-09-20 11:55:55 +0530

    trie implemented

--- trie/trie.h
+++ trie/trie.h
@@ -1,3 +1,65 @@
 #ifndef trie_h
 #define trie_h
+class trie
+{
+    static const int size = 256;
+    char c;
+    trie * child[size];
+    public:
+    trie()
+    {
+        c = -1;//not leaf 0 end of string
+        for(int i = 0;i<size;i++)
+        {
+            child[i] = NULL;
+        }
+    }
+    ~trie()
+    {
+        for(int i = 0;i<size;i++)
+        {
+            if(child[i] != NULL)
+            {
+                delete child[i];
+                child[i] = NULL;
+            }
+        }
+    }
+    void insert(const char*str)
+    {
+        if(*str == '\0')
+        {
+            c = 0;
+            return;
+        }
+        if(child[*str] == NULL)
+        {
+            child[*str] = new trie;
+        }
+        child[*str]->insert(str+1);
+    }
+    void insert(const string &s)
+    {
+        insert(s.c_str());
+    }
+    bool search(const char*str) const
+    {
+        if(str == '\0')
+        {
+            return true;
+        }
+        if(child[*str] != NULL)
+        {
+            if(*(str + 1) == '\0' && c == 0)
+                return true;
+            return child[*str]->search(str + 1);
+        }
+        return false;
+    }
+    bool search(const string &s) const
+    {
+        return search(s.c_str());
+    }
+
+};
 #endif
